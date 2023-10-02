f7ff3856f4c521a07b359c6d247eb631a84e6838
hdghaII <dr.mistical@gmail.com>
2023-10-02 18:00:07 +0300

    swiper

--- src/js/frontend.js
+++ src/js/frontend.js
@@ -1,1 +1,11 @@
 "use strict";
+document.addEventListener('DOMContentLoaded', () => {
+    var swiper = new Swiper(".swiper", {
+        slidesPerView: 3,
+        spaceBetween: 0,
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        }
+      });
+});
