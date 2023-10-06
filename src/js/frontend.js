a5b0a9433bfca47ec25c8528b23abcd4e284bff6
hdghaII <dr.mistical@gmail.com>
2023-10-06 12:17:16 +0300

    finished css

--- src/js/frontend.js
+++ src/js/frontend.js
@@ -2,10 +2,30 @@
 document.addEventListener('DOMContentLoaded', () => {
     var swiper = new Swiper(".swiper", {
         slidesPerView: 3,
-        spaceBetween: 0,
+        spaceBetween: 49,
         navigation: {
             nextEl: ".swiper-button-next",
             prevEl: ".swiper-button-prev",
+        },
+        breakpoints: {
+            // when window width is >= 320px
+            320: {
+                slidesPerView: 1,
+                spaceBetween: 0,
+                slideToClickedSlide: true,
+            },
+            // when window width is >= 480px
+            400: {
+                slidesPerView: 2,
+                spaceBetween: 0,
+                slideToClickedSlide: true,
+            },
+            // when window width is >= 640px
+            800: {
+                slidesPerView: 3,
+                spaceBetween: 49,
+                slideToClickedSlide: true,
+            },
         }
       });
 });
